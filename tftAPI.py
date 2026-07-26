b0dbbacf81bba88a58209421e894cfb761492e62
Toupetit2 <theo.doublet@yahoo.com>
2026-07-26 16:56:46 +0200

    Update tftAPI.py

--- tftAPI.py
+++ tftAPI.py
@@ -13,8 +13,9 @@ def get_tft_rank(puuid, cpid):
     url = f"https://{region}.api.riotgames.com/tft/league/v1/by-puuid/{puuid}"
 
     response = requests.get(url, headers=headers)
-
+    print(f"DEBUG - Riot API response for puuid {puuid} in region {region}: {response.status_code}", flush=True)
     if response.status_code == 200:
+        print(f"DEBUG - 200 code", flush=True)
         data = response.json()
         if data:
             if data[0]['tier'] != 'UNRANKED':
