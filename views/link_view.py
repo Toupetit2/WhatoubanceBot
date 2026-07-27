e3070468c44b1c1837c2d3e0cea53fff64f4e00a
Toupetit2 <theo.doublet@yahoo.com>
2026-07-27 18:45:02 +0200

    Update link_view.py

--- views/link_view.py
+++ views/link_view.py
@@ -15,7 +15,7 @@ class LinkView(discord.ui.View):
         self.discordAPI = discordAPI
         self.twitch_linker = twitch_linker
     
-    @discord.ui.button(label="🟥 Lier mon Riot", style=discord.ButtonStyle.gray, custom_id="riot_link_button") 
+    @discord.ui.button(label="🟥 Lier mon Riot", style=discord.ButtonStyle.red, custom_id="riot_link_button") 
     async def riot_link_button(self, interaction: discord.Interaction, button: discord.ui.Button): 
         discord_id = str(interaction.user.id) 
 
@@ -23,13 +23,13 @@ class LinkView(discord.ui.View):
         auth_url = f"https://auth.riotgames.com/authorize?client_id={RSO_CLIENT_ID}&redirect_uri={redirect_uri}&response_type=code&scope=openid+cpid&state={discord_id}&ui_locales=fr-FR"
         await interaction.response.send_message( f"👉 [Clique ici pour lier ton Riot]({auth_url})", ephemeral=True ) 
 
-    @discord.ui.button(label="🟪 Lier mon Twitch", style=discord.ButtonStyle.green, custom_id="twitch_link_button") 
+    @discord.ui.button(label="🟪 Lier mon Twitch", style=discord.ButtonStyle.primary, custom_id="twitch_link_button") 
     async def twitch_link_button(self, interaction: discord.Interaction, button: discord.ui.Button): 
         discord_id = str(interaction.user.id) 
         auth_url = self.twitch_linker.get_auth_url(discord_id) 
         await interaction.response.send_message( f"👉 [Clique ici pour lier ton Twitch]({auth_url})", ephemeral=True, suppress_embeds=True) 
 
-    @discord.ui.button(label="🟣 Notifs Twitch", style=discord.ButtonStyle.grey, custom_id="twitch_notification_button") 
+    @discord.ui.button(label="🟣 Notifs Twitch", style=discord.ButtonStyle.link, custom_id="twitch_notification_button") 
     async def twitch_notification_button(self, interaction: discord.Interaction, button: discord.ui.Button): 
         data = load_data()
         role = interaction.guild.get_role(data["twitch_notification_role"])
