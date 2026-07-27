78fba80882ffb7e508d3772dbe88265bbd814375
Toupetit2 <theo.doublet@yahoo.com>
2026-07-27 22:38:16 +0200

    Update update_rank.py

--- commands/update_rank.py
+++ commands/update_rank.py
@@ -40,7 +40,7 @@ def setup(bot):
     @app_commands.describe(member="Le membre qui va avoir son rank mis a jour")
     async def link_command(interaction: discord.Interaction, member: discord.Member):
 
-        message = await update_rank(interaction, member.id)
+        message = await update_rank(interaction, member)
 
         await interaction.response.send_message(message, ephemeral=True)
 
