b5f34667ff4feafd80f3103184f155808149fe22
Toupetit2 <theo.doublet@yahoo.com>
2026-07-30 12:43:00 +0200

    clean tracked files, apply gitignore

--- test.py
+++ test.py
@@ -2,15 +2,24 @@ import requests
 
 # nbMembres, rank, nbGames https://api-ggtech.leagueoflegends.com/api/v2/public/hubs/tft-clubs-fr/teams/whatoubance
 
-url = "https://api-ggtech.leagueoflegends.com/api/v2/public/showcase/tft-clubs-fr/teamMembers?visible=true&perPage=15&teamSlug=whatoubance&rol=staff&page=1"
+def in_wtb_club(riotID):
+    url = "https://api-ggtech.leagueoflegends.com/api/v2/public/showcase/tft-clubs-fr/teamMembers?visible=true&perPage=-1&teamSlug=whatoubance"
 
-response = requests.get(url)
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+    else:
+        print("Error GGtech API: ", response.text, flush=True)
 
-print("Status code :", response.status_code)
+    data = data["returnData"]["data"]
+    for player in data:
+            if player.get("gameNicks"):
+                if riotID == player["gameNicks"][0]["nick"]:
+                    return True
+            
+    return False
 
-if response.status_code == 200:
-    data = response.json()
-    print("Résultat JSON :")
-    print(data)
-else:
-    print("Erreur :", response.text)
+print(in_wtb_club("Toupetit2#WTB"))
+
+
